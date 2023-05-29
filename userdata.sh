63f14397f19af1a0e82556ded824c1573e8f9210
divay0710 <vimaldivay07@gmail.com>
2023-05-30 00:35:33 +0530

    as

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /opt
-git clone https://github.com/raghudevopsb72/roboshop-shell
+git clone https://github.com/divay0710/roboshop-shell
 cd roboshop-shell
 bash rabbitmq.sh ${rabbitmq_appuser_password} &>>/opt/roboshop.log
 
